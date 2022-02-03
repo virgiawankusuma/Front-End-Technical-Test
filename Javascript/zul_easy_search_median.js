59756e14e46d19c4ba27d8c1c2df461b165a591e
virgiawankusuma <virgiawan150@gmail.com>
2022-02-03 21:00:17 +0700

    feat: add code to find median from array

--- Javascript/zul_easy_search_median.js
+++ Javascript/zul_easy_search_median.js
@@ -3,7 +3,14 @@
 const input = [8, 7, 7, 9, 5, 4, 2, 9];
 
 function result(input) {
-  // Your Code Here
+  input.sort(function (a, b) {
+    return a - b;
+  });
+  if (input.length % 2 == 0) {
+    return (input[input.length / 2 - 1] + input[input.length / 2]) / 2;
+  } else {
+    return input[(input.length - 1) / 2];
+  }
 }
 
 console.log(result(input));
